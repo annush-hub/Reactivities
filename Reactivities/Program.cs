dca075eb17f012dff596422edaf95e5d6262e4b1
annushka <anna.bondarenko1@nure.ua>
2023-01-16 17:14:59 +0100

    client side getting all the activities

--- Reactivities/Program.cs
+++ Reactivities/Program.cs
@@ -15,6 +15,13 @@ builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors( opt => {
+    opt.AddPolicy("CorsPolicy", policy =>
+    {
+        policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -24,11 +31,15 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors("CorsPolicy");
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+
 using var scope = app.Services.CreateScope();
 var services = scope.ServiceProvider;
 
