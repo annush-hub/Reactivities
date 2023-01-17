71447a0f66ca5d20da6c2bcfa26e37d90011a9ef
annushka <anna.bondarenko1@nure.ua>
2023-01-17 11:41:00 +0100

    queries using MediatR

--- Reactivities/Controllers/BaseApiController.cs
+++ Reactivities/Controllers/BaseApiController.cs
@@ -1,11 +1,16 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Reactivities.Controllers
+namespace API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     public class BaseApiController : ControllerBase
     {
+        private IMediator _mediator;
+
+        protected IMediator Mediator => _mediator ??=
+            HttpContext.RequestServices.GetService<IMediator>();
     }
 }
