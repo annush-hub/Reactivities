40fcc1eb2381dce798f6b7fa11be99e9dc8478bd
annushka <anna.bondarenko1@nure.ua>
2023-01-18 10:37:26 +0100

    folder structure updated

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,6 +1,4 @@
 import React, { useEffect, useState } from "react";
-import logo from "./logo.svg";
-import "./App.css";
 import axios from "axios";
 import { Header, List } from "semantic-ui-react";
 import Button from "semantic-ui-react/dist/commonjs/elements/Button";
