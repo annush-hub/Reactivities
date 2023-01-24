82786068e7f0dd8169d80a065446f7e9b0b6d0e4
annushka <anna.bondarenko1@nure.ua>
2023-01-24 14:24:20 +0100

    Routes added

--- client-app/src/app/router/Routes.tsx
+++ client-app/src/app/router/Routes.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import { createBrowserRouter, RouteObject } from "react-router-dom";
+import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
+import ActivityFrom from "../../features/activities/form/ActivityFrom";
+import HomePage from "../../features/home/HomePage";
 import App from "../layout/App";
 
 export const routes: RouteObject[] = [
   {
     path: "/",
     element: <App />,
+    children: [
+      { path: "", element: <HomePage /> },
+      { path: "activities", element: <ActivityDashboard /> },
+      { path: "createActivity", element: <ActivityFrom /> },
+    ],
   },
 ];
 
