145d47851a1cca586d890268ba065a2f679d52bd
annushka <anna.bondarenko1@nure.ua>
2023-01-24 15:51:28 +0100

    Details Link added

--- client-app/src/app/router/Routes.tsx
+++ client-app/src/app/router/Routes.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { createBrowserRouter, RouteObject } from "react-router-dom";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
+import ActivityDetails from "../../features/activities/details/ActivityDetails";
 import ActivityFrom from "../../features/activities/form/ActivityFrom";
 import HomePage from "../../features/home/HomePage";
 import App from "../layout/App";
@@ -12,6 +13,7 @@ export const routes: RouteObject[] = [
     children: [
       { path: "", element: <HomePage /> },
       { path: "activities", element: <ActivityDashboard /> },
+      { path: "activities/:id", element: <ActivityDetails /> },
       { path: "createActivity", element: <ActivityFrom /> },
     ],
   },
