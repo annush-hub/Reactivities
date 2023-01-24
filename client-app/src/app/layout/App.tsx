cc9b754590f858c02ee1eca3bb65afb1ba280c0d
annushka <anna.bondarenko1@nure.ua>
2023-01-24 18:07:05 +0100

    HomePage moved outside the NavBar

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -2,16 +2,25 @@ import React, { Fragment, useEffect } from "react";
 import { Container } from "semantic-ui-react";
 import NavBar from "./NavBar";
 import { observer } from "mobx-react-lite";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
+import HomePage from "../../features/home/HomePage";
 
 function App() {
+  const location = useLocation();
+
   return (
-    <Fragment>
-      <NavBar />
-      <Container style={{ marginTop: "7em" }}>
-        <Outlet />
-      </Container>
-    </Fragment>
+    <>
+      {location.pathname === "/" ? (
+        <HomePage />
+      ) : (
+        <>
+          <NavBar />
+          <Container style={{ marginTop: "7em" }}>
+            <Outlet />
+          </Container>
+        </>
+      )}
+    </>
   );
 }
 
