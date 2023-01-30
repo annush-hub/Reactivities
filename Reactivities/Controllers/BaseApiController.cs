cba2bba90b9c3120939caebebc7328bdb85e1457
annushka <anna.bondarenko1@nure.ua>
2023-01-30 15:34:51 +0100

    Handling API Error responses while getting an Activity Details

--- Reactivities/Controllers/BaseApiController.cs
+++ Reactivities/Controllers/BaseApiController.cs
@@ -1,4 +1,5 @@
-﻿using MediatR;
+﻿using Application.Core;
+using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,5 +13,18 @@ namespace API.Controllers
 
         protected IMediator Mediator => _mediator ??=
             HttpContext.RequestServices.GetService<IMediator>();
+
+        protected ActionResult HandleResult<T>(Result<T> result)
+        {
+            if (result.IsSuccess && result.Value != null)
+            {
+                return Ok(result.Value);
+            }
+            if (result.IsSuccess && result.Value == null)
+            {
+                return NotFound();
+            }
+            return BadRequest(result.Error);
+        }
     }
 }
