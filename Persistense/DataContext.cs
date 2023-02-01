81b4924b7c14554d403971f62f11ac06846a0dd8
annushka <anna.bondarenko1@nure.ua>
2023-02-01 10:41:09 +0100

    IdentityDbContext added

--- Persistense/DataContext.cs
+++ Persistense/DataContext.cs
@@ -1,9 +1,10 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace Persistense
 {
-    public  class DataContext : DbContext
+    public  class DataContext : IdentityDbContext<AppUser>
     {
         public DataContext(DbContextOptions options) : base(options) 
         { 
