25f59014fbc8a451b0e28d1d3fdb91607ecc648a
annushka <anna.bondarenko1@nure.ua>
2023-02-03 15:02:24 +0100

    Sending up the token with the request done

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -1,6 +1,9 @@
-import axios, { AxiosResponse } from "axios";
+import axios, { AxiosError, AxiosHeaders, AxiosResponse } from "axios";
+import { toast } from "react-toastify";
 import { Activity } from "../models/activity";
 import { User, UserFormValues } from "../models/user";
+import { router } from "../router/Routes";
+import { store } from "../stores/store";
 
 const sleep = (delay: number) => {
   return new Promise((resolve) => {
@@ -10,17 +13,67 @@ const sleep = (delay: number) => {
 
 axios.defaults.baseURL = "https://localhost:7060/api";
 
-axios.interceptors.response.use(async (response) => {
-  try {
-    await sleep(1000);
-    return response;
-  } catch (error) {
-    console.log(error);
-    return await Promise.reject(error);
+const responseBody = <T>(response: AxiosResponse<T>) => response.data;
+
+axios.interceptors.request.use((config) => {
+  const token = store.commonStore.token;
+  if (token && config.headers) {
+    (config.headers as AxiosHeaders).set("Authorization", `Bearer ${token}`);
   }
+  return config;
 });
 
-const responseBody = <T>(response: AxiosResponse<T>) => response.data;
+// axios.interceptors.response.use(async (response) => {
+//   try {
+//     await sleep(1000);
+//     return response;
+//   } catch (error) {
+//     console.log(error);
+//     return await Promise.reject(error);
+//   }
+// });
+
+axios.interceptors.response.use(
+  async (response) => {
+    await sleep(1000);
+    return response;
+  },
+  (error: AxiosError) => {
+    const { data, status, config } = error.response as AxiosResponse;
+    switch (status) {
+      case 400:
+        if (config.method === "get" && data.errors.hasOwnProperty("id")) {
+          router.navigate("/not-found");
+        }
+        if (data.errors) {
+          const modalStateErrors = [];
+          for (const key in data.errors) {
+            if (data.errors[key]) {
+              modalStateErrors.push(data.errors[key]);
+            }
+          }
+          throw modalStateErrors.flat();
+        } else {
+          toast.error(data);
+        }
+        break;
+      case 401:
+        toast.error("unauthorised");
+        break;
+      case 403:
+        toast.error("forbidden");
+        break;
+      case 404:
+        router.navigate("/not-found");
+        break;
+      case 500:
+        store.commonStore.setServerError(data);
+        router.navigate("/server-error");
+        break;
+    }
+    return Promise.reject(error);
+  }
+);
 
 const requests = {
   get: <T>(url: string) => axios.get<T>(url).then(responseBody),
