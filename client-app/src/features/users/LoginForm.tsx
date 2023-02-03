0dfe8f056621717aab96f89aa978f0ead62510a8
annushka <anna.bondarenko1@nure.ua>
2023-02-03 11:02:00 +0100

    Displaying errors in the login form added

--- client-app/src/features/users/LoginForm.tsx
+++ client-app/src/features/users/LoginForm.tsx
@@ -1,7 +1,7 @@
-import { Form, Formik } from "formik";
+import { ErrorMessage, Form, Formik } from "formik";
 import { observer } from "mobx-react-lite";
 import React from "react";
-import { Button } from "semantic-ui-react";
+import { Button, Label } from "semantic-ui-react";
 import MyTextInput from "../../app/common/form/MyTextInput";
 import { useStore } from "../../app/stores/store";
 
@@ -10,11 +10,26 @@ export default observer(function LoginForm() {
 
   return (
     <Formik
-      initialValues={{ email: "", password: "" }}
-      onSubmit={(values) => userStore.login(values)}
+      initialValues={{ email: "", password: "", error: null }}
+      onSubmit={(values, { setErrors }) =>
+        userStore
+          .login(values)
+          .catch((error) => setErrors({ error: "Invalid email or password" }))
+      }
     >
-      {({ handleSubmit, isSubmitting }) => (
+      {({ handleSubmit, isSubmitting, errors }) => (
         <Form className="ui form" onSubmit={handleSubmit} autoComplete="off">
+          <ErrorMessage
+            name="error"
+            render={() => (
+              <Label
+                style={{ marginBottom: 10 }}
+                basic
+                color="red"
+                content={errors.error}
+              ></Label>
+            )}
+          />
           <MyTextInput placeholder="Email" name="email" />
           <MyTextInput placeholder="Password" name="password" type="password" />
           <Button
