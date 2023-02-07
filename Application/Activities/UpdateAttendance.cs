2016f70b9a3334b94a00fea63e949a1a62db1e13
annushka <anna.bondarenko1@nure.ua>
2023-02-07 08:45:40 +0100

    The attendance handler added

---
+++ Application/Activities/UpdateAttendance.cs
@@ -0,0 +1,78 @@
+﻿using Application.Core;
+using Application.Interfaces;
+using Domain;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistense;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Activities
+{
+    public class UpdateAttendance
+    {
+        public class Command : IRequest<Result<Unit>>
+        {
+            public Guid Id { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Command, Result<Unit>>
+        {
+            private readonly DataContext _context;
+            private readonly IUserAccessor _userAccessor;
+
+            public Handler(DataContext context, IUserAccessor userAccessor)
+            {
+                _context = context;
+                _userAccessor = userAccessor;
+            }
+
+            public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var activtiy = await _context.Activities
+                    .Include(a => a.Attendees).ThenInclude(u => u.AppUser)
+                    .FirstOrDefaultAsync(x => x.Id == request.Id);
+
+                if (activtiy == null) return null;
+
+                var user = await _context.Users.FirstOrDefaultAsync(x =>
+                x.UserName == _userAccessor.GetUserName());
+
+                if (user == null) return null;
+
+                var hostUsername = activtiy.Attendees.FirstOrDefault(x => x.IsHost)?.AppUser.UserName;
+
+                var attendance = activtiy.Attendees.FirstOrDefault(x => x.AppUser.UserName == user.UserName);
+
+                if (attendance != null & hostUsername == user.UserName)
+                {
+                    activtiy.IsCancelled = !activtiy.IsCancelled;
+                }
+
+                if (attendance != null && hostUsername != user.UserName)
+                {
+                    activtiy.Attendees.Remove(attendance);
+                }
+
+                if (attendance == null)
+                {
+                    attendance = new ActivityAttendee
+                    {
+                        AppUser = user,
+                        Activity = activtiy,
+                        IsHost = false,
+                    };
+
+                    activtiy.Attendees.Add(attendance);
+                }
+                
+                var result = await _context.SaveChangesAsync() > 0;
+                return result ? Result<Unit>.Success(Unit.Value)
+                    : Result<Unit>.Failure("Problem updating attendance");
+            }
+        }
+    }
+}
--- Reactivities/Controllers/ActivitiesController.cs
+++ Reactivities/Controllers/ActivitiesController.cs
@@ -22,14 +22,14 @@ namespace API.Controllers
         [HttpGet("{id}")]
         public async Task<IActionResult> GetActivity(Guid id)
         {
-            var result= await Mediator.Send(new Details.Query { Id = id});
+            var result = await Mediator.Send(new Details.Query { Id = id });
             return HandleResult(result);
         }
 
         [HttpPost]
         public async Task<IActionResult> CreateActivity(Activity activity)
         {
-            return HandleResult(await Mediator.Send(new Create.Command { Activity = activity}));
+            return HandleResult(await Mediator.Send(new Create.Command { Activity = activity }));
         }
 
 
@@ -44,6 +44,12 @@ namespace API.Controllers
         public async Task<IActionResult> DeleteActivity(Guid id)
         {
             return HandleResult(await Mediator.Send(new Delete.Command { Id = id }));
+        }
+
+        [HttpPost("{id}/attend")]
+        public async Task<IActionResult> Attend(Guid id)
+        {
+            return HandleResult(await Mediator.Send(new UpdateAttendance.Command { Id = id }));
         }
     }
 }
