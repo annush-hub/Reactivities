90c6112465da0a177f31a14ab329f23d2b56c226
annushka <anna.bondarenko1@nure.ua>
2023-02-07 15:43:32 +0100

    SeedData updated

---
+++ Persistense/Migrations/20230207142912_SeedsUpdated.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistense.Migrations
+{
+    public partial class SeedsUpdated : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ Persistense/Migrations/20230207144104_FinalSeedData.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistense.Migrations
+{
+    public partial class FinalSeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
