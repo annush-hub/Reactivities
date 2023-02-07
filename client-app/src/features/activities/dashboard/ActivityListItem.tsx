1879e25a8158ace6e8be96febfb241a6b6cb3192
annushka <anna.bondarenko1@nure.ua>
2023-02-07 23:37:09 +0100

    A popup for Attendees added

--- client-app/src/features/activities/dashboard/ActivityListItem.tsx
+++ client-app/src/features/activities/dashboard/ActivityListItem.tsx
@@ -45,7 +45,12 @@ export default function ActivityListItem({ activity }: Props) {
         )}
         <ItemGroup>
           <Item>
-            <Item.Image size="tiny" circular src="/assets/user.png" />
+            <Item.Image
+              style={{ marginBottom: 3 }}
+              size="tiny"
+              circular
+              src="/assets/user.png"
+            />
             <Item.Content>
               <Item.Header as={Link} to={`/activities/${activity.id}`}>
                 {activity.title}
