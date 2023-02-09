532799114b1e1497a0e7ff2dcffd452ffe953f49
annushka <anna.bondarenko1@nure.ua>
2023-02-09 13:27:22 +0100

    Getting user's profile + setting the main profile image done

--- Application/Core/MappingProfiles.cs
+++ Application/Core/MappingProfiles.cs
@@ -18,6 +18,9 @@ namespace Application.Core
                 .ForMember(d => d.DisplayName, o => o.MapFrom(s => s.AppUser.DisplayName))
                 .ForMember(d => d.Username, o => o.MapFrom(s => s.AppUser.UserName))
                 .ForMember(d => d.Bio, o => o.MapFrom(s => s.AppUser.Bio));
+
+            CreateMap<AppUser, Profiles.Profile>()
+                .ForMember(d => d.Image, o => o.MapFrom(S => S.Photos.FirstOrDefault(x => x.IsMain).Url));
         }
     }
 }
